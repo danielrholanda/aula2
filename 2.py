1217a85b3646f621e4df4de1ac14c99b32f7a502
danielrholanda <danielrholanda@gmail.com>
2025-07-11 20:41:21 -0300

    Add files via upload

---
+++ 2.py
@@ -0,0 +1,9 @@
+#posso fazer comentário
+print('oi') # irá imprimir oi na tela 
+#docString
+"""
+comentario de bloco
+é desse jeito que
+será ignorado pelo 
+programa
+"""
---
+++ 4.py
@@ -0,0 +1,10 @@
+"""
+python - linguagem de programação
+tipos de dados
+"""
+
+print('Daniel')
+print("Daniel")
+print(1,'Daniel')
+print(2, "Daniel 'Aluno' ")
+print(2, 'Daniel "Aluno" ')
