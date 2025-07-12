0ef5cfe984b12429c4887a857d89f44c2d5d278f
danielrholanda <danielrholanda@gmail.com>
2025-07-11 21:53:00 -0300

    Add files via upload

---
+++ 7.py
@@ -0,0 +1,9 @@
+#variaveis são usadas para salvar
+#algo na memória do computador
+#uso name_variavel = expressao
+nome = 'Daniel'
+sobrenome = 'Holanda'
+print('Nome :',nome)
+print ('Sobrenome :',sobrenome)
+concatenacao = 'Daniel'+'  ' 'Holanda'
+print(concatenacao)
---
+++ 8.py
@@ -0,0 +1,9 @@
+nome = 'Daniel'
+idade = 31
+altura = 1.73
+peso = 76
+
+print('Nome:', nome,'\n Idade :',idade,'\n')
+imc = peso / altura**2 
+print('Nome:', nome,'\n Idade :',idade,'\nIMC :',imc)
+print('IMC :', imc)
